b6bbf4b1ffec6a80d0d42df75e0f245fdbbd656a
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-05 17:23:41 -0300

    feat - Adiciona service de usuário, e função de login sem implementação

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
+  private destroy$ = new Subject<void>();
 
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    senha: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder,) {
+  }
+
+  submitLoginForm(): void {
+    console.log("DADOS DO FORM", this.loginForm.value);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
---
+++ src/app/services/usuario/usuario.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/enviroment';
+import { LoginRequest } from '../../models/interfaces/usuario/auth/LoginRequest';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../../models/interfaces/usuario/auth/LoginResponse';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  autenticaUsuario(loginRequest: LoginRequest) : Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.API_URL}/auth/login`, loginRequest);
+  }
+}
