3981bdfff21c0f3b54e8885d5dff0537817dc840
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-11 10:01:31 -0300

    feat - Adiciona funcionalidade de logout

--- src/app/modules/shared/components/sidebar-navigation/sidebar-navigation.component.ts
+++ src/app/modules/shared/components/sidebar-navigation/sidebar-navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sidebar-navigation',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar-navigation.component.scss'
 })
 export class SidebarNavigationComponent {
-  sidebarVisible: boolean = false
+  sidebarVisible: boolean = false;
+
+  constructor(private cookie: CookieService, private router: Router) {}
+
+  handleLogOut(): void {
+    this.cookie.delete('USUARIO_INFO');
+
+    this.router.navigate(['/home']);
+  }
 }
