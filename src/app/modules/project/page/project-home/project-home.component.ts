daef89a2732be0452a2fd40158d47d82c47eded9
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-11 11:08:00 -0300

    feat - Implementa função de listar projetos no componente de projetos

--- src/app/modules/project/page/project-home/project-home.component.ts
+++ src/app/modules/project/page/project-home/project-home.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { ProjectService } from '../../../../services/project/project.service';
+import { MessageService } from 'primeng/api';
+import { ProjectResponse } from '../../../../models/interfaces/project/ProjectResponse';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-home',
   templateUrl: './project-home.component.html',
   styleUrl: './project-home.component.scss'
 })
-export class ProjectHomeComponent {
+export class ProjectHomeComponent implements OnInit, OnDestroy{
+  private destroy$: Subject<void> = new Subject;
+  public projectsDatas: Array<ProjectResponse> = [];
 
+  constructor(
+    private projectService: ProjectService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getProjectDatas();
+  }
+
+  getProjectDatas(): void {
+    this.projectService
+    .getAllProjects()
+    .pipe(takeUntil(this.destroy$))
+    .subscribe({
+      next: (response) => {
+        if(response.length > 0) {
+          this.projectsDatas = response;
+        }
+      },
+      error: (err) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Erro',
+          detail: 'Erro ao buscar projetos',
+          life: 2500,
+        });
+        this.router.navigate(['/dashboard']);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
