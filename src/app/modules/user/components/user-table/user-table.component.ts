f378e7fae67b3d5d609bba7dc54edab66cbca856
Vinicius Azambuja <vinizambs@gmail.com>
2025-02-24 11:25:17 -0300

    feat - Adiciona funcionalidade de abrir Dialog para registrar usuários

--- src/app/modules/user/components/user-table/user-table.component.ts
+++ src/app/modules/user/components/user-table/user-table.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserResponse } from '../../../../models/interfaces/user/UserResponse';
+import { EventAction } from '../../../../models/interfaces/events/EventAction';
+import { UserEvent } from '../../../../models/enums/user/UserEvent';
 
 @Component({
   selector: 'app-user-table',
@@ -8,6 +10,17 @@ import { UserResponse } from '../../../../models/interfaces/user/UserResponse';
 })
 export class UserTableComponent {
   @Input() users: Array<UserResponse> = [];
+  @Output() userEvent = new EventEmitter<EventAction>();
 
   public userSelected!: UserResponse;
+
+  public addUserEvent = UserEvent.ADD_USER_EVENT;
+  public editUserEvent = UserEvent.EDIT_USER_EVENT;
+
+  handleUserEvent(action: string, id?: number) {
+   if(action && action !== '') {
+     const userEventData = id ? {action, id} : {action};
+     this.userEvent.emit(userEventData);
+   }
+  }
 }
--- src/app/modules/user/page/user-home/user-home.component.ts
+++ src/app/modules/user/page/user-home/user-home.component.ts
@@ -4,6 +4,9 @@ import { UserResponse } from '../../../../models/interfaces/user/UserResponse';
 import { UserService } from '../../../../services/user/user.service';
 import { MessageService } from 'primeng/api';
 import { Router } from '@angular/router';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { EventAction } from '../../../../models/interfaces/events/EventAction';
+import { UserFormComponent } from '../../components/user-form/user-form.component';
 
 @Component({
   selector: 'app-user-home',
@@ -13,8 +16,10 @@ import { Router } from '@angular/router';
 export class UserHomeComponent {
   private destroy$: Subject<void> = new Subject;
   public usersDatas: Array<UserResponse> = [];
+  private ref!: DynamicDialogRef;
 
   constructor(
+    private dialogService: DialogService,
     private userService: UserService,
     private messageService: MessageService,
     private router: Router
@@ -44,6 +49,27 @@ export class UserHomeComponent {
         this.router.navigate(['/dashboard']);
       },
     });
+  }
+
+  handleUserAction(event: EventAction): void {
+    if(event) {
+      this.ref = this.dialogService.open(UserFormComponent, {
+        header: event?.action,
+        width: '70%',
+        contentStyle: { overflow: 'auto'},
+        baseZIndex: 10000,
+        maximizable: true,
+        data: {
+          event: event.action,
+          usersDatas: this.usersDatas,
+        },
+      });
+      this.ref.onClose
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: () => this.getUserDatas(),
+        });
+    }
   }
 
   ngOnDestroy(): void {
